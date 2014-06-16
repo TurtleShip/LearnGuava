588dc335b58fa445423927d8ee1df44754f42455
Seulgi Kim <sk765@cornell.edu>
2014-06-15 20:22:02 -0700

    Wrote tests for Sets.

--- src/test/java/com/seulgi/collections/SetsTest.java
+++ src/test/java/com/seulgi/collections/SetsTest.java
@@ -1,6 +1,7 @@
 package com.seulgi.collections;
 
 import com.google.common.collect.Sets;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Set;
@@ -10,17 +11,50 @@ import static org.hamcrest.Matchers.is;
 
 public class SetsTest {
 
+    private Set<String> s0;
+    private Set<String> s1;
+
+    @Before
+    public void setUp() {
+        s0 = Sets.newHashSet("1", "2", "3");
+        s1 = Sets.newHashSet("2", "3", "4");
+    }
+
     @Test
     public void testDifference() {
-        Set<String> s0 = Sets.newHashSet("1", "2", "3");
-        Set<String> s1 = Sets.newHashSet("2", "3", "4");
         Set<String> diff0 = Sets.difference(s0, s1).immutableCopy();
         Set<String> diff1 = Sets.difference(s1, s0).immutableCopy();
         Set<String> expectedDiff0 = Sets.newHashSet("1");
         Set<String> expectedDiff1 = Sets.newHashSet("4");
 
-        // TODO: Find a better way to compare two sets
-        assertThat(expectedDiff0.containsAll(diff0) && diff0.containsAll(expectedDiff0), is(true));
-        assertThat(expectedDiff1.containsAll(diff1) && diff1.containsAll(expectedDiff1), is(true));
+        assertThat(diff0, is(expectedDiff0));
+        assertThat(diff1, is(expectedDiff1));
+    }
+
+    @Test
+    public void testSymmetricDifference() {
+        /*
+        The Sets.symmetricDifference method returns elements that are contained in one set or the other set,
+        but not contained in both. The returned set is an unmodifiable view.
+         */
+        Set<String> symDiff = Sets.symmetricDifference(s0, s1);
+        Set<String> expectedDiff = Sets.newHashSet("1", "4");
+
+        assertThat(symDiff, is(expectedDiff));
+    }
+
+    @Test
+    public void testIntersection() {
+        Set<String> inter = Sets.intersection(s0, s1);
+        Set<String> expected = Sets.newHashSet("2", "3");
+        assertThat(inter, is(expected));
+    }
+
+    @Test
+    public void testUnion() {
+        Set<String> union = Sets.union(s0, s1);
+        Set<String> expected = Sets.newHashSet("1", "2", "3", "4");
+
+        assertThat(union, is(expected));
     }
 }
